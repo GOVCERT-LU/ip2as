7b23f3254c7678d588c4f65fd3240c1acb8e3f7b
Georges Toth <georges.toth@govcert.etat.lu>
2012-10-15 01:24:04 +0200

    load input into class for easier processing

--- ip2as.py
+++ ip2as.py
@@ -29,8 +29,8 @@ class IP2AS(object):
       m = re.match(r'^(\d+)\|([^|]+)\|(.*)$', l)
       if m:
         net = m.group(2)
-        text = l.rstrip()
-        self.net_as.insert(net, text)
+        ipdescr = IPDescr(l.rstrip())
+        self.net_as.insert(net, ipdescr)
       else:
         # @TODO what to do if no match
         # should not happen but does happen
@@ -38,12 +38,37 @@ class IP2AS(object):
 
   def get(self, ip):
     try:
-      return self.net_as[ip]
+      return self.net_as[ip].text
     except:
       return ''
+
+  def getobj(self, ip):
+    try:
+      return self.net_as[ip]
+    except:
+      return None
+
+
+class IPDescr(object):
+  def __init__(self, ipas_line):
+    # 8966|88.221.217.0/24|ETISALAT-AS Emirates Telecommunications Corporation|EU|ripencc|20060201
+    self.asn = ''
+    self.net = ''
+    self.descr = ''
+    self.cc = ''
+    self.rir = ''
+    self.update = ''
+
+    self.setdata(ipas_line)
+
+  def setdata(self, ipas_line):
+    self.asn, self.net, self.descr, self.cc, self.rir, self.update = ipas_line.split('|')
+    self.text = ipas_line
+    return ipas_line
 
 
 if __name__ == '__main__':
   ip2as = IP2AS('net_as')
 
   print ip2as.get('194.154.205.135')
+  print ip2as.getobj('194.154.205.135').asn
