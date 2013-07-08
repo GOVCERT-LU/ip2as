51ca9924eb83f0d9d97759b2113183040784cffe
Georges Toth <georges.toth@govcert.etat.lu>
2013-07-08 13:38:37 +0200

    major rework

---
+++ ip2as/api.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Georges Toth'
+__email__ = 'georges.toth@govcert.etat.lu'
+__copyright__ = 'Copyright 2013, Georges Toth'
+__license__ = 'GPL v3+'
+
+#
+# IP2AS client API
+#
+
+import json
+import urllib2
+
+
+
+class IP2ASApi(object):
+  def __init__(self, api_url, api_key, http_timeout=5, verify_ssl=False, proxies={}):
+    self.api_url = api_url
+    self.api_key = api_key
+    self.verify_ssl = verify_ssl
+    self.proxies = proxies
+
+    if len(proxies) > 0:
+      proxy = urllib2.ProxyHandler(proxies)
+      opener = urllib2.build_opener(proxy)
+      urllib2.install_opener(opener)
+
+  def __request(self, method, data, extra_headers=None):
+    url = '{0}/{1}'.format(self.api_url, method)
+    headers = {'Content-Type': 'application/json; charset=utf-8', 'key' : self.api_key}
+
+    if extra_headers:
+      for k, v in extra_headers.items():
+        headers[k] = v
+
+    r = urllib2.Request(url, data=json.dumps(data), headers=headers)
+    try:
+      res = urllib2.urlopen(r).read()
+    except urllib2.HTTPError, e:
+      raise Exception('Error ({0})'.format(e.code))
+    except urllib2.URLError, e:
+      raise Exception('Error ({0})'.format(e.reason.args[1]))
+
+    # @FIXME why do we need to load json twice? -> doing it once returns an unquoted string O_o
+    res = json.loads(res)
+
+    return json.loads(res)
+
+  def get_asn(self, asn):
+    return self.__request('get_asn', {'asn' : asn})
+
+  def get_ip(self, ip):
+    return self.__request('get_ip', {'ip' : ip})
+
+
+def json_pretty_print(j):
+  return json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))
